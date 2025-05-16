f4882c00762b87e4fd17f764ab0ce3140b8eb931
DoNBaLooN <124310871+DoNBaLooN@users.noreply.github.com>
2025-05-16 19:46:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,11 @@ WWW_DIR="/www"
 echo "Загружаем файлы с GitHub..."
 wget -O "$WWW_DIR/ip.html" "$REPO_URL/ip.html"
 wget -O "$WWW_DIR/wanip.sh" "$REPO_URL/wanip.sh"
+wget -O "$WWW_DIR/reset_dns.sh" "$REPO_URL/reset_dns.sh"
 
 echo "Делаем wanip.sh исполняемым..."
 chmod +x "$WWW_DIR/wanip.sh"
+chmod +x "$WWW_DIR/reset_dns.sh"
 
 echo "Проверяем настройку uhttpd на поддержку .sh..."
 if ! uci get uhttpd.main.interpreter 2>/dev/null | grep -q "/bin/sh"; then
