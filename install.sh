f21e78391e12f29d0ce4c412c4950e00e0540ff1
DoNBaLooN <124310871+DoNBaLooN@users.noreply.github.com>
2025-05-18 21:50:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-REPO_URL="https://raw.githubusercontent.com/DoNBaLooN/router-info-page/main"  # Укажи свой URL
+REPO_URL="https://raw.githubusercontent.com/DoNBaLooN/router-info-page/main"
 WWW_DIR="/www"
 
 # Создаём необходимые каталоги
@@ -10,22 +10,22 @@ mkdir -p "$WWW_DIR/cgi-bin"
 
 # Загружаем основные файлы в WWW_DIR
 echo "Загружаем файлы в $WWW_DIR..."
-wget -O "$WWW_DIR/ip.html"         "$REPO_URL/ip.html"
-wget -O "$WWW_DIR/countries.html" "$REPO_URL/countries.html"
-wget -O "$WWW_DIR/wanip.sh"       "$REPO_URL/wanip.sh"
-wget -O "$WWW_DIR/set_time.sh"    "$REPO_URL/set_time.sh"
-wget -O "$WWW_DIR/reset_dns.sh"   "$REPO_URL/reset_dns.sh"
-wget -O "$WWW_DIR/get_time.sh"    "$REPO_URL/get_time.sh"
+wget -O "$WWW_DIR/ip.html"           "$REPO_URL/ip.html"
+wget -O "$WWW_DIR/countries.html"   "$REPO_URL/countries.html"
+wget -O "$WWW_DIR/wanip.sh"         "$REPO_URL/wanip.sh"
+wget -O "$WWW_DIR/set_time.sh"      "$REPO_URL/set_time.sh"
+wget -O "$WWW_DIR/reset_dns.sh"     "$REPO_URL/reset_dns.sh"
+wget -O "$WWW_DIR/get_time.sh"      "$REPO_URL/get_time.sh"
 
 # Загружаем файл подписок
 echo "Загружаем subscriptions.txt в $WWW_DIR/country..."
-wget -O "$WWW_DIR/country/subscriptions.txt" "$REPO_URL/subscriptions.txt"
+wget -O "$WWW_DIR/country/subscriptions.txt" "$REPO_URL/country/subscriptions.txt"
 
 # Загружаем CGI-скрипты
 echo "Загружаем CGI-скрипты в $WWW_DIR/cgi-bin..."
-wget -O "$WWW_DIR/cgi-bin/upload_subscriptions.sh" "$REPO_URL/upload_subscriptions.sh"
-wget -O "$WWW_DIR/cgi-bin/set_proxy.sh"           "$REPO_URL/set_proxy.sh"
-wget -O "$WWW_DIR/cgi-bin/countries.sh"           "$REPO_URL/countries.sh"
+wget -O "$WWW_DIR/cgi-bin/upload_subscriptions.sh" "$REPO_URL/cgi-bin/upload_subscriptions.sh"
+wget -O "$WWW_DIR/cgi-bin/set_proxy.sh"           "$REPO_URL/cgi-bin/set_proxy.sh"
+wget -O "$WWW_DIR/cgi-bin/countries.sh"           "$REPO_URL/cgi-bin/countries.sh"
 
 # Делаем все скрипты исполняемыми
 echo "Делаем скрипты исполняемыми..."
